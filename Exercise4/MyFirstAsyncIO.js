9ede2426bf1a9ed23aebce403a2858f3e8141fc8
Calum Munro <camunro03@gmail.com>
2017-02-23 12:52:44 +0000

    Completed exercise4, first asyn example code.

---
+++ Exercise4/MyFirstAsyncIO.js
@@ -0,0 +1,35 @@
+var fs = require('fs')
+var numberOfNewLines = undefined
+
+function calculateNumberOfNewLines(callback) {
+  fs.readFile(process.argv[2], function doneReading(err, fileContents) {
+    if (err) {
+        return console.log(err)
+      }
+    var fileContentsAsString = fileContents.toString();
+    numberOfNewLines = parseInt(fileContentsAsString.split("\n").length) - 1;
+    callback()
+  })
+}
+
+function logNumberOfNewLines() {
+  console.log(numberOfNewLines)
+}
+
+calculateNumberOfNewLines(logNumberOfNewLines)
+
+
+// Official Solution
+/*    
+    var fs = require('fs')
+    var file = process.argv[2]
+
+    fs.readFile(file, function (err, contents) {
+      if (err) {
+        return console.log(err)
+      }
+      // fs.readFile(file, 'utf8', callback) can also be used
+      var lines = contents.toString().split('\n').length - 1
+      console.log(lines)
+    })
+*/
