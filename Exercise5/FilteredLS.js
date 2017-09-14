c9d06b81107e704dc6cf8521789861b50a661926
Calum Munro <camunro03@gmail.com>
2017-09-14 11:36:41 +0100

    Completed exercise 5! Yaldi.

--- Exercise5/FilteredLS.js
+++ Exercise5/FilteredLS.js
@@ -1,22 +1,36 @@
 var fs = require('fs');
 var path = require('path');
 var inputDirectory = process.argv[2]
-var fileType = process.argv[3]
+var localFileType = process.argv[3]
 
+fs.readdir(inputDirectory, function PrintAllDirectories(err, files) {
 
-function GetAllDirectoriesInLocation(callback) {
-   var directories = fs.readdir(inputDirectory, callback(files))
-}
-
-function PrintAllDirectories(err, listOfDirectories) {
-   
     if(err) {
-     console.log('Unknown Error occured when reading from the directory ${inputDirectory}')
-   }
-
-    for (var i = 0; i < listOfDirectories.length; i++) {
-        console.log(listOfDirectories[i])
+        console.log('Unknown Error occured when reading from the directory ${inputDirectory}')
     }
-}
 
-GetAllDirectoriesInLocation()
+    for (var i = 0; i < files.length; i++) {
+
+        var currentFile = files[i];
+
+        if (path.extname(currentFile) == ('.' + localFileType)) {
+            console.log(currentFile)
+        }
+    }
+})
+
+// Actual Solution
+//
+// var fs = require('fs')
+// var path = require('path')      
+//
+// var folder = process.argv[2]     
+// var ext = '.' + process.argv[3]      
+//
+// fs.readdir(folder, function (err, files) {       
+//     if (err) return console.error(err)       
+//     files.forEach(function (file) {         
+//         if (path.extname(file) === ext) {           
+//             console.log(file)         }       
+//     })     
+// })
